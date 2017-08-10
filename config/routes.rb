fe8fb3454458ad4df24d1ee32e49a9ce33039309
Kurt Tamulonis <kurttamulonis@gmail.com>
2017-08-10 09:58:32 -0400

    innitial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'setup/index'
+  get '/setup/open' => 'setup#open'
+  post '/setup/generate' => 'setup#generate'
+  get '/setup' => 'setup#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
